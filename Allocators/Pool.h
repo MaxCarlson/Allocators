2d4aea8c7e09ca28566f70897360b5bf837e8c39
Maxwell Carlson <bart4282819@gmail.com>
2018-09-24 00:15:36 -0700

    Created the outline of the pool allocator and various policies that will be used for giving out chunks of memory

---
+++ Allocators/Pool.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <list>
+#include <memory>
+
+
+namespace alloc
+{
+	template<size_t bytes>
+	struct ListPolicy
+	{
+
+	};
+
+	template<size_t bytes>
+	struct TreePolicy
+	{
+
+	};
+
+	template<class Type, size_t bytes = 0, template<size_t> class Policy = ListPolicy>
+	class Pool
+	{
+		Policy<bytes> storage;
+	};
+}
--- Allocators/main.cpp
+++ Allocators/main.cpp
@@ -1,7 +1,7 @@
 #include <memory>
 #include "Linear.h"
 #include "FixedBlock.h"
-
+#include "Pool.h"
 
 // Just a temporary main to test allocators from
 // Should be removed in any actual use case
