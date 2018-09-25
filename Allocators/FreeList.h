d5418f833faf52edffc04122f27660f0bff10993
Maxwell Carlson <bart4282819@gmail.com>
2018-09-24 23:46:34 -0700

    Forgot to include headers when allocating memory so we can deallocate. Will do that, as well as list integration tomorrow

--- Allocators/FreeList.h
+++ Allocators/FreeList.h
@@ -22,7 +22,7 @@ namespace alloc
 	{
 		inline static byte* MyBegin;
 		inline static size_t MyLast;
-		inline static std::list<std::pair<byte*, size_t>> availible;
+		inline static std::list<std::pair<byte*, size_t>> availible; // TODO: These list nodes should be contained in memory before allocated blocks?
 
 		inline static bool init			= 1;
 		inline static AlSearch search	= FIRST_FIT;
@@ -44,7 +44,7 @@ namespace alloc
 			return nullptr;
 		}
 
-		void addToList(byte* start, size_t size)
+		void addToList(byte const* start, size_t size)
 		{
 			if (availible.empty())
 			{
@@ -73,7 +73,6 @@ namespace alloc
 					auto itBytes = std::move(it->second);
 					availible.erase(it);
 
-
 					// If memory section is larger than what we want
 					// add a new list entry the reflects the remaining memory
 					if (itBytes > byteCount)
@@ -87,8 +86,7 @@ namespace alloc
 		template<class T>
 		T* allocate(size_t count)
 		{
-			byte* mem;
-
+			byte* mem = nullptr;
 			const size_t byteCount = sizeof(T) * count;
 
 			if (search == FIRST_FIT)
@@ -100,8 +98,17 @@ namespace alloc
 				throw std::bad_alloc();
 
 			MyLast += byteCount;
-
 			return reinterpret_cast<T*>(mem);
+		}
+
+		// BUG: Issue here. Not keeping track of allocated block length
+		// don't know how much to free
+		//
+		// TODO: To fix add a header to all allocations ( be sure to include header offsets in all ops )
+		template<class T>
+		void deallocate(T const* ptr)
+		{
+			//addToList(ptr, )
 		}
 	};
 
@@ -120,5 +127,9 @@ namespace alloc
 			return storage.allocate<Type>(count);
 		}
 
+		void deallocate(Type const* ptr)
+		{
+			storage.deallocate(ptr);
+		}
 	};
 }
